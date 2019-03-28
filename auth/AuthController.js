114dad4bf0587004cc302e907337a42336e4be1e
mmiazi <sakibnm@gmail.com>
2019-03-28 18:50:56 -0400

    v1

--- auth/AuthController.js
+++ auth/AuthController.js
@@ -7,6 +7,7 @@ var User = require('../user/User');
 var jwt = require('jsonwebtoken');
 var bcrypt = require('bcryptjs');
 var config = require('../config');
+var VerifyToken = require('./VerifyToken');
 
 router.post('/register', function(req, res){
     var hashedPassword = bcrypt.hashSync(req.body.password, 8);
@@ -26,20 +27,25 @@ router.post('/register', function(req, res){
     });
 });
 
-router.get('/me', function(req, res) {
+router.get('/me', VerifyToken, function(req, res, next) {
     var token = req.headers['x-access-token'];
     if (!token) return res.status(401).send({ auth: false, message: 'No token provided.' });
     
     jwt.verify(token, config.secret, function(err, decoded) {
       if (err) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
       
-      User.findById(decoded.id, {password:0}, function (err, user) {
+      User.findById(req.userId, {password:0}, function (err, user) {
         if (err) return res.status(500).send("There was a problem finding the user.");
         if (!user) return res.status(404).send("No user found.");
         
         res.status(200).send(user);
+        //next(user);
       });
     });
+});
+
+router.use((user, req, res, next)=>{
+  res.status(200).send("user"); 
 });
 
 router.post('/login', function(req, res) {
